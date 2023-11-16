273f6ca155bf7eea66328712588d9707f6f7c08c
AMADOUSARR <amadou.sarr@bdl.lu>
2023-11-16 15:33:12 +0100

    Added VetService

--- pet-clinic-data/src/main/java/com/petclinicapp/services/VetService.java
+++ pet-clinic-data/src/main/java/com/petclinicapp/services/VetService.java
@@ -1,4 +1,11 @@
 package com.petclinicapp.services;
 
+import com.petclinicapp.model.Vet;
+
+import java.util.Set;
+
 public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
 }
