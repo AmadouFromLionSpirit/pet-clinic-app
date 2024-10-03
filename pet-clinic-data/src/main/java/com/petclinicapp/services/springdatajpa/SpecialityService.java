62d4c44730d653aa070e413fad15b5d42d170caa
Sarr <contact@amadousarr.com>
2024-10-03 12:19:29 +0200

    Added Spring Data JPA Vet Speciality Service Implementation

---
+++ pet-clinic-data/src/main/java/com/petclinicapp/services/springdatajpa/SpecialityService.java
@@ -0,0 +1,7 @@
+package com.petclinicapp.services.springdatajpa;
+
+import com.petclinicapp.model.Speciality;
+import com.petclinicapp.services.CrudService;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
